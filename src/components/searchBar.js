e93b1ce45fb3614ca0176313111e2226cdb4c869
patrykkoziel <patrykkoziel99@o2.pl>
2020-11-16 06:59:19 +0100

    Update searchBar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -42,7 +42,7 @@ class SearchComponent extends Component {
         method: "GET",
         headers: {
           Authorization:
-            "Client-ID iN9GMgPKIiLYCHBH3kRRovvF2-YAB-CQoCxQwSa6fx0",
+            "Client-ID",
         },
       }
     )
